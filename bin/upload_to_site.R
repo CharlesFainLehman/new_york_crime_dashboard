daf5b39c14bbadc77065886ad035af4291d1df92
Charles Fain Lehman <clehman@manhattan.institute>
2024-09-26 09:32:11 -0400

    trying to push site again

--- bin/upload_to_site.R
+++ bin/upload_to_site.R
@@ -1,3 +1,3 @@
 library(rsconnect)
-rsconnect::setAccountInfo(name='manhattan-institute', token='0AFD0942A9173E026C2B630AD17DE92F', secret=commandArgs(trailingOnly=TRUE)[1])
+rsconnect::setAccountInfo(name='manhattan-institute', token='D7E98B14F95BD4122499A048795045DF', secret=commandArgs(trailingOnly=TRUE)[1])
 rsconnect::deployApp(forceUpdate = TRUE)
